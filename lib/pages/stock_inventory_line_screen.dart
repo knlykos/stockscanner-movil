0e823b7bccacb54e4260445fd4d3de0c77e0d107
Nefi López García <nlopezg87@gmail.com>
2019-11-02 11:57:09 -0700

    inventory.line con estructura basica

--- lib/pages/stock_inventory_line_screen.dart
+++ lib/pages/stock_inventory_line_screen.dart
@@ -54,12 +54,12 @@ class StockInventoryLineScreen extends StatelessWidget {
       body: StockInventoryLineListView(
         inventoryId: inventoryId,
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('hola');
-        },
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.add),
+      //   onPressed: () {
+      //     print('hola');
+      //   },
+      // ),
     );
   }
 }
@@ -79,6 +79,9 @@ class _StockInventoryLineListViewState
   List<dynamic> data;
   List<dynamic> products;
   int length;
+  TextEditingController searchController;
+  ServerProvider serverProvider;
+  Future<OdooResponse> productList;
 
   Image imageFromBase64String(String base64String) {
     if (base64String != null) {
@@ -92,95 +95,90 @@ class _StockInventoryLineListViewState
     products = [];
     length = 0;
     // TODO: implement initState
+    searchController = new TextEditingController();
+
     super.initState();
+  }
+
+  Future<OdooResponse> getOdooStockInventoryLine() async {
+    OdooResponse response;
+    client = new OdooClient('https://odoo.nkodexsoft.com');
+
+    final auth = await client.authenticate(serverProvider.userDB,
+        serverProvider.passwordDB, serverProvider.selectedDB);
+    print(auth.isSuccess);
+    if (auth.isSuccess) {
+      // print(auth.getUser());
+      final domain = [
+        ['inventory_id', '=', widget.inventoryId]
+      ];
+      // print(domain);
+      final fields = null;
+      final res = await client.searchRead(
+        "stock.inventory.line",
+        domain,
+        fields,
+      );
+      if (!res.hasError()) {
+        // if (!res.hasError()) {
+        // final dataOdoo = response.getResult();
+        // // print(dataOdoo['length']);
+        // this.data = dataOdoo['records'];
+        // print(this.data);
+        // this.data.map((f) => {print(f)});
+        // print({'ENTRIES', response.getResult()['records'].asMap().entries});
+        print(res.getResult());
+        for (var entry in res.getResult()['records'].asMap().entries) {
+          // print({'entry', entry.value['inventory_id'][0]});
+          final domain = [
+            ['id', '=', entry.value['product_id'][0]]
+          ];
+          print(domain);
+          const fields = null;
+          OdooResponse product =
+              await client.searchRead('product.product', domain, fields);
+          // print(product.getResult());
+          res.getResult()['records'][0]['product'] =
+              product.getResult()['records'];
+          //   response.getResult()['records'][i]['product'] =
+          //       product.getResult()['records'];
+          //   print(response.getResult()['records'][i]['product']);
+          // });
+
+          // client.searchRead('product.product', [['id', '=', this.data[]]], fields)
+        }
+        // response.getResult()['records'].asMap().forEach((i, v) async {
+        //   // products.add(v);
+        //   // response.getResult()['records'][i]['product'] = v;
+        //   // this.data[i]['product'] = v;
+
+        // } else {
+        //   // print(result.getError());
+        // }
+        return res;
+      } else {
+        return res;
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final serverProvider = Provider.of<ServerProvider>(context);
-    Future<OdooResponse> getOdooStockInventoryLine() async {
-      OdooResponse response;
-      client = new OdooClient('https://odoo.nkodexsoft.com');
-
-      final auth = await client.authenticate(serverProvider.userDB,
-          serverProvider.passwordDB, serverProvider.selectedDB);
-      print(auth.isSuccess);
-      if (auth.isSuccess) {
-        // print(auth.getUser());
-        final domain = [
-          ['inventory_id', '=', widget.inventoryId]
-        ];
-        // print(domain);
-        final fields = null;
-        final res = await client.searchRead(
-          "stock.inventory.line",
-          domain,
-          fields,
-        );
-        if (!res.hasError()) {
-          // if (!res.hasError()) {
-          // final dataOdoo = response.getResult();
-          // // print(dataOdoo['length']);
-          // this.data = dataOdoo['records'];
-          // print(this.data);
-          // this.data.map((f) => {print(f)});
-          // print({'ENTRIES', response.getResult()['records'].asMap().entries});
-          print(res.getResult());
-          for (var entry in res.getResult()['records'].asMap().entries) {
-            // print({'entry', entry.value['inventory_id'][0]});
-            final domain = [
-              ['id', '=', entry.value['product_id'][0]]
-            ];
-            print(domain);
-            const fields = null;
-            OdooResponse product =
-                await client.searchRead('product.product', domain, fields);
-            // print(product.getResult());
-            res.getResult()['records'][0]['product'] =
-                product.getResult()['records'];
-            //   response.getResult()['records'][i]['product'] =
-            //       product.getResult()['records'];
-            //   print(response.getResult()['records'][i]['product']);
-            // });
-
-            // client.searchRead('product.product', [['id', '=', this.data[]]], fields)
-          }
-          // response.getResult()['records'].asMap().forEach((i, v) async {
-          //   // products.add(v);
-          //   // response.getResult()['records'][i]['product'] = v;
-          //   // this.data[i]['product'] = v;
-
-          // } else {
-          //   // print(result.getError());
-          // }
-          return res;
-        } else {
-          return res;
-        }
-      }
-    }
-
     // getOdooStockInventoryLine();
-
-    return FutureBuilder(
-        future: getOdooStockInventoryLine(),
+    serverProvider = Provider.of<ServerProvider>(context);
+    productList ??= getOdooStockInventoryLine();
+    var futureBuilder = FutureBuilder(
+        future: productList,
         builder: (context, AsyncSnapshot<OdooResponse> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            // print(snapshot.data.getResult());
-            // print(snapshot.data.getResult()['records']);
-            // print(this.products[0]);
-            // print([data]);
-            // print({'length', length});
             final records = snapshot.data.getResult()['records'];
             final length = snapshot.data.getResult()['length'];
             return ListView.builder(
               itemCount: length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title:
-                      Text(records[index]['product'][0]['display_name']),
-                  subtitle:
-                      Text(records[index]['product'][0]['create_date']),
+                  title: Text(records[index]['product'][0]['display_name']),
+                  subtitle: Text(records[index]['product'][0]['create_date']),
                   trailing: Icon(Icons.chevron_right),
                   onTap: () {
                     Navigator.push(
@@ -198,6 +196,30 @@ class _StockInventoryLineListViewState
             return Container(child: Text('Cargando'));
           }
         });
+    return Container(
+        child: Column(
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 30,
+          color: Colors.grey,
+          child: TextField(
+            decoration: InputDecoration(border: InputBorder.none),
+            textAlign: TextAlign.center,
+            controller: searchController,
+            keyboardType: TextInputType.number,
+          ),
+        ),
+        Flexible(
+          child: futureBuilder,
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: 30,
+          color: Colors.red,
+        ),
+      ],
+    ));
 
     // return Container();
   }
